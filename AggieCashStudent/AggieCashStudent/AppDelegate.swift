80cb767481310aabe1946d83fa44e11579f539ba
Liyin Li <knyli@ucdavis.edu>
2020-01-26 11:21:12 -0800

    add new project

---
+++ AggieCashStudent/AggieCashStudent/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  AggieCashStudent
+//
+//  Created by Kenny Li on 1/26/20.
+//  Copyright © 2020 UC Davis ECS 199 Winter 2020. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ AggieCashStudent/AggieCashStudent/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  AggieCashStudent
+//
+//  Created by Kenny Li on 1/26/20.
+//  Copyright © 2020 UC Davis ECS 199 Winter 2020. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
