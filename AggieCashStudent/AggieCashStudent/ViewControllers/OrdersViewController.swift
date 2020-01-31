86a91c488e8701147811fd71fd19262da4be0eb5
aadila6 <aabudureheman@gmail.com>
2020-01-30 21:39:56 -0800

    Update few more UI components

---
+++ AggieCashStudent/AggieCashStudent/ViewControllers/OrdersViewController.swift
@@ -0,0 +1,30 @@
+//
+//  OrdersViewController.swift
+//  AggieCashStudent
+//
+//  Created by Adila on 1/30/20.
+//  Copyright © 2020 AggieCashStudent. All rights reserved.
+//
+
+import UIKit
+
+class OrdersViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
