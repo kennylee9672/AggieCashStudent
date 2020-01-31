a6fe4c6d2b52804f8363d5041223ab724d1603b0
Davy <dchuon@ucdavis.edu>
2020-01-30 19:41:35 -0800

    cleaning up code

--- AggieCashStudent/AggieCashStudent/AppDelegate.swift
+++ AggieCashStudent/AggieCashStudent/AppDelegate.swift
@@ -18,11 +18,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         FirebaseApp.configure()
-//        GIDSignIn.sharedInstance().clientID = FirebaseApp.app()?.options.clientID
-//        GIDSignIn.sharedInstance().delegate = self
+        //        GIDSignIn.sharedInstance().clientID = FirebaseApp.app()?.options.clientID
+        //        GIDSignIn.sharedInstance().delegate = self
         return true
     }
     
+    // For Google Sign In to handle the URL the app receives after authentication
     @available(iOS 9.0, *)
     func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool {
         let handled = GIDSignIn.sharedInstance().handle(url)
@@ -30,6 +31,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // return GIDSignIn.sharedInstance().handle(url,
         // sourceApplication:options[UIApplication.OpenURLOptionsKey.sourceApplication] as? String,
         // annotation: [:])
+    }
+    
+    // Same as above function, but for iOS 8 and older
+    func application(_ application: UIApplication, open url: URL, sourceApplication: String?, annotation: Any) -> Bool {
+        return GIDSignIn.sharedInstance().handle(url)
     }
     
     // MARK: UISceneSession Lifecycle
@@ -45,61 +51,5 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
     
-    //    // For Google Sign In to handle the URL the app receives after authentication
-    //    @available(iOS 9.0, *)
-    //    func application(_ application: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any])
-    //        -> Bool {
-    //            return GIDSignIn.sharedInstance().handle(url)
-    //    }
-    //
-    //    // Same as above function, but for iOS 8 and older
-    //    func application(_ application: UIApplication, open url: URL, sourceApplication: String?, annotation: Any) -> Bool {
-    //        return GIDSignIn.sharedInstance().handle(url)
-    //    }
-    //
-    //
-    //    func sign(_ signIn: GIDSignIn!, didSignInFor user: GIDGoogleUser!, withError error: Error?) {
-    //        // ...
-    //        if let error = error {
-    //            // ...
-    //            print(error)
-    //            return
-    //        } else {
-    //            let userToken = user.authentication.idToken
-    //            let userId = user.userID
-    //            let userFullName = user.profile.name
-    //            let userEmail = user.profile.email
-    //            print(userFullName)
-    //            print(userEmail)
-    //        }
-    //
-    //        guard let authentication = user.authentication else { return }
-    //        let credential = GoogleAuthProvider.credential(withIDToken: authentication.idToken,
-    //                                                       accessToken: authentication.accessToken)
-    //
-    //        Auth.auth().signInAndRetrieveData(with: credential) { (authResult, error) in
-    //            if let error = error {
-    //                print("Firebase ERROR")
-    //                print(error)
-    //                return
-    //            }
-    //            print("User successfully signed in with Firebase")
-    //
-    //            // navigate to home view
-    //            let homeViewController = self.storyboard?.instantiateViewController(withIdentifier: "HomeTabBarViewController") as? UITabBarController
-    //
-    //            self.view.window?.rootViewController = homeViewController
-    //            self.view.window?.makeKeyAndVisible()
-    //
-    //        }
-    //
-    //
-    //        // ...
-    //    }
-    //
-    //    func sign(_ signIn: GIDSignIn!, didDisconnectWith user: GIDGoogleUser!, withError error: Error!) {
-    //        // Perform any operations when the user disconnects from app here.
-    //        // ...
-    //    }
 }
 
